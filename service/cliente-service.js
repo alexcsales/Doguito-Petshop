a2b5e46cdbae358d8100bb0f8f60fb59332cf9df
Alex <alexcsalesdev@gmail.com>
2023-04-26 11:12:56 -0300

    modificado: adicionado o uso de fetch

--- service/cliente-service.js
+++ service/cliente-service.js
@@ -15,22 +15,11 @@ const criaNovaLinha = (nome, email) => {
 const tabela = document.querySelector('[data-tabela]')
 
 const listaClientes = () => {
-    const promise = new Promise((resolve, reject) => {
-        const http = new XMLHttpRequest()
 
-        http.open('GET', 'http://localhost:3000/profile')
-
-        http.send()
-
-        http.onload = () => {
-            if (http.status >= 400) {
-                reject(JSON.parse(http.response))
-            } else {
-                resolve(JSON.parse(http.response))
-            }
-        }
-    })
-    return promise
+    return fetch('http://localhost:3000/profile')
+        .then(resposta => {
+            return resposta.json()
+        })
 }
 
 listaClientes()
